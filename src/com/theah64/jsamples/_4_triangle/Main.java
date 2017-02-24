1280eccd3c101b2ef37cfafcbf39cd2ce083f2c4
theapache64-hotel <theapache64@gmail.com>
2017-02-24 07:49:38 +0530

    Finished

--- src/com/theah64/jsamples/_4_triangle/Main.java
+++ src/com/theah64/jsamples/_4_triangle/Main.java
@@ -15,7 +15,28 @@ public class Main {
         double b = s.nextInt();
         double c = s.nextInt();
 
-        if
+        if ((a + b) > c && (a + c) > b && (b + c) > a) {
+
+            System.out.println("Triangle can form");
+            if ((a == b) && (b == c) && (a == c)) {
+                System.out.println("Triangle is equalateral");
+            }
+
+            if ((a == b) && (a != c) || (a == c) && (a != b) || (b == c) && (b != a)) {
+                System.out.println("Trainle is isosceles");
+            }
+
+            if ((a != b) && (a != c) && (b != c)) {
+                System.out.println("Triangle is scalene");
+            }
+            double e = (a + b + c) / 2;
+            double area = Math.sqrt(e * (e - a) * (e - b) * (e - c));
+            System.out.println("Area of the triangle : " + area);
+
+
+        } else {
+            System.out.println("Triangle cannot form");
+        }
 
     }
 }
